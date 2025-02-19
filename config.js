f19661173f684c72a52ef17ebca6fcad98b3ab34
fmvaldezg <fmvaldezg@yahoo.com>
2025-02-19 10:56:26 -0500

    updated draft

--- config.js
+++ config.js
@@ -6,17 +6,79 @@ var config = {
     legend: false, // if legend is set to true, inset will be disabled.
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps. You will need to provide your own terrain tiles.
-    useCustomLayers: true, //set true for enabling custom layers from sources.js
+    useCustomLayers: false, //set true for enabling custom layers from sources.js
     bookmarks: true,
     chapterReturn: true,
     auto: false,
-    //title: 'A tour of Temple University Campuses',
+    title: 'A tour of Temple University Campuses',
     //logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Temple_University_Logo.svg/320px-Temple_University_Logo.svg.png",
-    //subtitle: 'Demo for the fall 2024 workshop',
-    //byline: 'By Felipe Valdez',
+    subtitle: 'Demo for the fall 2024 workshop',
+    byline: 'By Felipe Valdez',
     mobileview: '<div id="rotate-mobile"><em>For optimal viewing of this storytelling map on mobile, rotate your device to a horizontal orientation.</em><br><br><img src="images/device.png">', // to add custom messaging in the header for mobile devices
     footer: 'Source: source citations, etc.<br> Created using <a href="https://github.com/digidem/maplibre-storymap" target="_blank">MapLibre Storytelling</a> template.',
     chapters: [
+        {
+            id: 'blank',
+            alignment: 'left',
+            hidden: true,
+            //title: '',
+            //image: '',
+            //caption: '',
+            //website: '',
+            //author: '',
+            //legend: '',
+            //description: '',
+            location: {
+                center: [-99.78614, 39.74777],
+                zoom: 3.53,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: true,
+            callback: '',
+            //onChapterEnter: [
+                 //{
+                   //  layer: '1875_poly',
+                  //   opacity: 1,
+                 //    duration: 5000
+                 //}
+
+            //],
+            //onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            //]
+        },
+        {
+            id: 'intro',
+            alignment: 'left',
+            hidden: false,
+            //title: 'Introduction',
+            image: '',
+            caption: '',
+            //website: '<a href="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd" target="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd">Historical map</a>',
+            //author: 'Felipe Valdez',
+            //legend: '<span style="font-size: 0.85em;"><em>Legend content</em><br></span><img src="/images/legend1.png" style="display: block; margin-top: 10px; width: 100%; max-width: 200px;">',
+            description: '...add descritpion text',
+            location: {
+                center: [-76.72589, 39.84370],
+                zoom: 6.54,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: true,
+            callback: '',
+            //onChapterEnter: [],
+            //onChapterExit: []
+        },
         {
             id: '1875',
             alignment: 'left',
@@ -27,9 +89,9 @@ var config = {
             //website: '<a href="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd" target="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd">Historical map</a>',
             //author: 'Felipe Valdez',
             //legend: '<span style="font-size: 0.85em;"><em>Legend content</em><br></span><img src="/images/legend1.png" style="display: block; margin-top: 10px; width: 100%; max-width: 200px;">',
-            description: 'Temple’s bustling Main Campus crackles with energy, thanks to its location in the heart of Philadelphia, numerous new building projects and the 10,000 students who now live on or around our increasingly residential campus. On- and off-campus activities abound, including more than 300 student organizations; Division I sporting events; and nearby restaurants, shops and entertainment. <iframe width="100%" height="800px" src="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd" frameborder="0" scrolling="no"></iframe>',
+            description: '..description',
             location: {
-                center: [-75.18187, 39.99027],
+                center: [-75.183, 39.99027],
                 zoom: 16.12,
                 pitch: 0,
                 bearing: 0
@@ -87,53 +149,6 @@ var config = {
            ]
         },
         {
-            id: 'chapter3',
-            alignment: 'left',
-            hidden: false,
-            title: 'Center City',
-            image: './images/image3.png',
-            website: '<a href="https://universitycollege.temple.edu/center-city" target="https://universitycollege.temple.edu/center-city">Website</a>', 
-            description: 'Temple University Center City (TUCC) offers the ideal learning environment for adult and continuing-education students who must often balance the demands of a career, family and school. Surrounded by the home offices of major national corporations and just across the street from Philadelphia’s City Hall. Additionally, TUCC is near many of the city’s cultural and entertainment resources. Classes are offered primarily in the evenings and on weekends.',
-            location: {
-                center: [-75.16602897661596, 39.95294851795492],
-                zoom: 17,
-                pitch: 52,
-                bearing: -36.70
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: true,
-            mapInteractive: false,
-            callback: '',
-            onChapterEnter: [],
-           onChapterExit: []
-        },
-        {
-            id: 'chapter4',
-            alignment: 'center',
-            hidden: false,
-            title: 'Health Sciences Center',
-            image: './images/image4.png',
-            website: '<a href="https://universitycollege.temple.edu/center-city" target="https://universitycollege.temple.edu/center-city">Website</a>', 
-            description: 'Step onto the Health Sciences Center and be immersed in the future of healthcare. Home to four of Temple’s medical-related schools and colleges, as well as the nationally ranked Temple University Hospital, the Health Sciences Center is a hub of academic, clinical and research activities.',
-            location: {
-                center: [-75.15191, 40.00617],
-                zoom: 16,
-                pitch: 61.70,
-                bearing: 61.70
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: true,
-            mapInteractive: false,
-            callback: '',
-            onChapterEnter: [
-            //     {
-            //         layer: 'pcwnet',
-            //         opacity: 1
-            //     }
-            ],
-            onChapterExit: []
-        },
-        {
             id: 'chapter5',
             alignment: 'right',
             hidden: false,
@@ -174,32 +189,6 @@ var config = {
                 pitch: 0,
                 bearing: 0,
                 speed: 0.7
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            mapInteractive: true,
-            callback: '',
-            onChapterEnter: [
-            //     {
-            //         layer: 'pcwnet',
-            //         opacity: 1
-            //     }
-            ],
-            onChapterExit: []
-        }, 
-        {
-            id: 'chapter7',
-            alignment: 'center',
-            hidden: false,
-            title: 'Workshop',
-            image: './images/image7.png',
-            //website: '<a href="https://studyabroad.temple.edu/programs/temple-university-rome/semester-academic-year-summer" target="https://studyabroad.temple.edu/programs/temple-university-rome/semester-academic-year-summer">Website</a>', 
-            description: 'Do you want to learn how to create your own story map like this? Visit our hands on <a href="https://felipevaldez.com/storytelling_maplibre_workshop/" target="https://felipevaldez.com/storytelling_maplibre_workshop/">online tutorial.</a>',
-            location: {
-                center: [-75.15515, 39.98185],
-                zoom:2.27,
-                pitch: 0,
-                bearing: 0
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
